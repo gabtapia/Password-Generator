e18207e4eba72bb682e6e2880cb90595bc3eb0bf
Gabriel_Coder <82976811+gabtapia@users.noreply.github.com>
2022-04-18 17:47:16 -0300

    JS Adicionado

---
+++ script.js
@@ -0,0 +1,16 @@
+function generatePW() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById('pw').value = password
+}
+
+function copyPW() {
+  document.querySelector('#pw').select();
+  document.execCommand("copy");
+}
